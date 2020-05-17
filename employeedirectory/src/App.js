53274bb362ce611de983ecc97c2910af4850ea0d
Abelard Bautista <abelardbautista@gmail.com>
2020-05-16 22:59:58 -0700

    Moved App.js out of components into src root.

---
+++ employeedirectory/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/App.sass";
+import { Navbar } from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
--- employeedirectory/src/components/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "../styles/App.sass";
-import { Navbar } from "./Navbar";
-
-function App() {
-  return <Navbar />;
-}
-
-export default App;
