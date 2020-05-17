31aaa24c706ad27386ab480a7ba980ed4edfae40
Abelard Bautista <abelardbautista@gmail.com>
2020-05-17 00:43:49 -0700

    Fixed class to className.

--- employeedirectory/src/components/Navbar.js
+++ employeedirectory/src/components/Navbar.js
@@ -22,7 +22,7 @@ export const Navbar = () => {
           <a className="navbar-item has-text-weight-bold is-size-4">
             Employee Navigator
           </a>
-          <a class="navbar-item">GitHub</a>
+          <a className="navbar-item">GitHub</a>
           <div className="navbar-item has-dropdown is-hoverable">
             <a className="navbar-link">More</a>
             <div className="navbar-dropdown">
