c375152165fd1e3e1584cdb6c64fb4f4dbf46341
Abelard Bautista <abelardbautista@gmail.com>
2020-05-16 22:54:16 -0700

    created separate component for Navbar.

--- employeedirectory/src/components/App.js
+++ employeedirectory/src/components/App.js
@@ -1,40 +1,9 @@
 import React from "react";
 import "../styles/App.sass";
+import { Navbar } from "./Navbar";
 
 function App() {
-  return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <a
-          role="button"
-          className="navbar-burger burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start">
-          <a className="navbar-item has-text-weight-bold is-size-4">
-            Employee Navigator
-          </a>
-          <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link">More</a>
-
-            <div className="navbar-dropdown">
-              <a className="navbar-item">GitHub</a>
-              <a className="navbar-item">Portfolio</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+  return <Navbar />;
 }
 
 export default App;
