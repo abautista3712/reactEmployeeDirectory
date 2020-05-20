fbe4c555a44cb5e972a6b5da7f161a642c0a752a
Abelard Bautista <abelardbautista@gmail.com>
2020-05-19 22:21:41 -0700

    Click on burger prints to console.

--- employeedirectory/src/components/Navbar.js
+++ employeedirectory/src/components/Navbar.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Navbar = () => {
+  function handleClick(e) {
+    e.preventDefault();
+    console.log("The link was clicked");
+  }
+
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
+        <a className="navbar-item has-text-weight-bold is-size-4">
+          Employee Navigator
+        </a>
         <a
           role="button"
           className="navbar-burger burger"
           aria-label="menu"
           aria-expanded="false"
           data-target="navbarBasicExample"
+          onClick={handleClick}
         >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -17,16 +26,12 @@ export const Navbar = () => {
         </a>
       </div>
 
-      <div id="navbarBasicExample" className="navbar-menu">
+      <div id="navbarBasicExample" className="navbar-menu is-active">
         <div className="navbar-start">
-          <a className="navbar-item has-text-weight-bold is-size-4">
-            Employee Navigator
-          </a>
           <a className="navbar-item">GitHub</a>
           <div className="navbar-item has-dropdown is-hoverable">
             <a className="navbar-link">More</a>
             <div className="navbar-dropdown">
-              <a className="navbar-item">Developer GitHub</a>
               <a className="navbar-item">Portfolio</a>
             </div>
           </div>
