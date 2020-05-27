2d24be1e182aa94b2aedd09f79243bf06e85e782
Abelard Bautista <abelardbautista@gmail.com>
2020-05-26 23:48:55 -0700

    Moved code to separate component.

---
+++ employeedirectory/src/components/AlphabeticalSort.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
+
+export const AlphabeticalSort = () => {
+  const [employees, setEmployees] = useState({ results: [] });
+
+  useEffect(() => {
+    API.getUsers().then((res) => {
+      setEmployees(res.data);
+    });
+  }, []);
+
+  return employees.results
+    .sort((a, b) => {
+      let nameA = a.name.first;
+      let nameB = b.name.first;
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    })
+    .map((employee, index) => (
+      <tr>
+        <td>
+          <img src={employee.picture.medium} alt="Employee Thumbnail" />
+        </td>
+        <td>{`${employee.name.first} ${employee.name.last}`}</td>
+        <td>{employee.email}</td>
+        <td>{employee.cell}</td>
+        <td>{`${employee.location.street.number} ${employee.location.street.name}`}</td>
+        <td>{employee.location.city}</td>
+        <td>{employee.location.state}</td>
+        <td>{employee.location.country}</td>
+      </tr>
+    ));
+};
